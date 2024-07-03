a874b9a53a9b95219018cd33e593fb127f95c656
zhusisheng <zhusisheng@shenhaoinfo.com>
2024-07-03 09:32:42 +0800

    fix: add new data

--- app/blog/component/Tag.tsx
+++ app/blog/component/Tag.tsx
@@ -4,7 +4,7 @@ import { TagModel } from 'types/index.types'
 const Tag = ({ data }: { data: TagModel }) => {
   return (
     <Link
-      href={`/tags/`}
+      href={`/blog?page=1&tagId=${data.id}`}
       className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
     >
       {data.name}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,105 @@
-import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
-import { allBlogs } from 'contentlayer/generated'
-import Main from './Main'
+import Link from '@/components/Link'
+import siteMetadata from '@/data/siteMetadata'
+import { formatDate } from 'pliny/utils/formatDate'
+import NewsletterForm from 'pliny/ui/NewsletterForm'
+import { alovaInstance } from 'http/index'
+import { ArticleModel } from 'types/index.types'
+import Tag from './blog/component/Tag'
 
 export default async function Page() {
-  const sortedPosts = sortPosts(allBlogs)
-  const posts = allCoreContent(sortedPosts)
-  return <Main posts={posts} />
+  const {
+    data: {
+      list,
+      pageData: { pageCount },
+    },
+  } = await alovaInstance.Get<{ data: { list: ArticleModel[]; pageData: { pageCount: number } } }>(
+    `https://nestjs.zsjs.fun/article/page`,
+    {
+      params: {
+        page_index: 1,
+        page_size: 10,
+      },
+      localCache: 1000,
+      next: { revalidate: 0 },
+    }
+  )
+
+  return (
+    <>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Latest
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            {siteMetadata.description}
+          </p>
+        </div>
+        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+          {!list.length && 'No posts found.'}
+          {list.map((post) => {
+            return (
+              <li key={post.id} className="py-12">
+                <article>
+                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                    <dl>
+                      <dt className="sr-only">Published on</dt>
+                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                        <time dateTime={post.updatedAt}>{formatDate(post.updatedAt, 'en-US')}</time>
+                      </dd>
+                    </dl>
+                    <div className="space-y-5 xl:col-span-3">
+                      <div className="space-y-6">
+                        <div>
+                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                            <Link
+                              href={`/blog/${post.id}`}
+                              className="text-gray-900 dark:text-gray-100"
+                            >
+                              {post.title}
+                            </Link>
+                          </h2>
+                          <div className="flex flex-wrap">
+                            {post.tags?.map((tag) => <Tag data={tag} key={tag.id} />)}
+                          </div>
+                        </div>
+                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                          {post.summary}
+                        </div>
+                      </div>
+                      <div className="text-base font-medium leading-6">
+                        <Link
+                          href={`/blog/${post.id}`}
+                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          aria-label={`Read more: "${post.title}"`}
+                        >
+                          Read more &rarr;
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      {list.length && (
+        <div className="flex justify-end text-base font-medium leading-6">
+          <Link
+            href="/blog"
+            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            aria-label="All posts"
+          >
+            All Posts &rarr;
+          </Link>
+        </div>
+      )}
+      {/* {siteMetadata.newsletter?.provider && (
+        <div className="flex items-center justify-center pt-4">
+          <NewsletterForm />
+        </div>
+      )} */}
+    </>
+  )
 }
